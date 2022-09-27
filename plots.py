a761264323c89c739cec12a97a4268d53bfec859
Nolan Smyth <nolanwsmyth@gmail.com>
2022-09-27 10:34:01 -0700

    Improving plotting

--- plots.py
+++ plots.py
@@ -5,16 +5,26 @@ import h5py
 from scipy.integrate import quadrature
 import pickle
 import pandas as pd
+import warnings
+from matplotlib.ticker import MultipleLocator, NullFormatter
+
+warnings.filterwarnings("ignore")
 
 # Load interpolations
 pk_sd_interp = pickle.load(open("interps/pks_sd_interp.p", "rb"))
 pk_dd_interp = pickle.load(open("interps/pks_dd_interp.p", "rb"))
 
+# paths to styles and data
 plt.style.use("/Users/nolansmyth/Dropbox/kinetic_recoupling/figures/style.mplstyle")
+h5pydir = "/Users/nolansmyth/Dropbox/kinetic_recoupling/h5py_dat/"
 
 # Constants for plotting
 pk_max = 1e2
 kk = np.logspace(-4, np.log10(pk_max), 500)
+
+# Which A_recs to use for delta recoupling rate
+A_recs = [1e8, 10 ** (8.5), 1e9]
+k = 10  # use one k mode for now. This is the only one I generated data for
 
 
 def dmu_idm_dr(
@@ -312,3 +322,214 @@ def plot_observations(Tr, Ar, best_fit_a, mwarm):
     plt.savefig(plot_dir + filename)
     plt.clf()
 
+
+def plot_delta_recoupling_rate():
+
+    # zs used for plotting
+    z_plot = np.geomspace(1e4, 2e7, int(1e5))
+
+    for i, A_rec in reversed(list(enumerate(A_recs))):
+        plt.plot(
+            z_plot,
+            kappa_dot_zs_arr[i](z_plot),
+            label="A_rec = " + scientific_format(A_rec),
+        )
+
+    plt.plot(z_data_no_rec, kappa_dot_data_no_rec, label="no rec ")
+
+    plt.plot([1e-3, 1e10], [1, 1], "k--")
+    plt.plot([1e-3, 1e10], [1e-3, 1e-3], "k:")
+    plt.plot([1e-3, 1e10], [1e3, 1e3], "k:")
+
+    plt.xlim(1e6, 6e7)
+    plt.ylim(1e-4, 1e5)
+
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlabel("z", fontsize=16)
+    plt.ylabel("$\Gamma_{\mathrm{DR-DM}}$", fontsize=16)
+    plt.title("Comoving Scattering Rate")
+    plt.legend()
+
+    plot_dir = "Figures/"
+    filename = "Scattering_rate_delta.pdf"
+    plt.savefig(plot_dir + filename)
+    plt.clf()
+
+
+# Get data for no recoupling
+spline_pars = {"k": 3, "s": 0.0}
+data_file = h5pydir + "class_model_data_no_rec.hdf5"
+with h5py.File(data_file, "r") as f:
+    tau_data = np.array(f["scalar"]["k=" + str(k)]["tau [Mpc]"])
+    delta_chi_data = np.array(f["scalar"]["k=" + str(k)]["delta_idm_dr"])
+    phi_data = np.array(f["scalar"]["k=" + str(k)]["phi"])
+    theta_dr_data = np.array(f["scalar"]["k=" + str(k)]["theta_idr"])
+    theta_data = np.array(f["scalar"]["k=" + str(k)]["theta_idm_dr"])
+    Pks_no_rec = np.array(f["power_spectrum"]["Pk"])
+    kks_no_rec = np.array(f["power_spectrum"]["kk"])
+    kappa_dot_data_no_rec = np.flip(np.array(f["thermodynamics"]["dmu_idm_dr"]))
+    z_data_no_rec = np.flip(np.array(f["thermodynamics"]["z"]))
+    rho_idr_no_rec = np.array(f["background"]["(.)rho_idr"])
+    rho_idm_no_rec = np.array(f["background"]["(.)rho_idm_dr"])
+    bkg_zs_no_rec = np.array(f["background"]["z"])
+
+delta_chi_no_rec = UnivariateSpline(tau_data, delta_chi_data, **spline_pars)
+delta_chi_dot_no_rec = delta_chi_no_rec.derivative()
+phi_no_rec = UnivariateSpline(tau_data, phi_data, **spline_pars)
+phi_dot_no_rec = phi_no_rec.derivative()
+theta_dr_no_rec = UnivariateSpline(tau_data, theta_dr_data, **spline_pars)
+theta_chi_no_rec = UnivariateSpline(tau_data, theta_data, **spline_pars)
+theta_chi_dot_no_rec = theta_chi_no_rec.derivative()
+
+# Get data for recouplings
+delta_chi_arr = []
+delta_chi_dot_arr = []
+delta_chi_ddot_arr = []
+phi_dot_arr = []
+phi_ddot_arr = []
+Pk_arr = []
+kappa_dot_taus_arr = []
+kappa_dot_zs_arr = []
+z_arr = []
+tau_arr = []
+thermo_tau_arr = []
+theta_dr_arr = []
+rho_idr_arr = []
+rho_idm_arr = []
+theta_chi_dot_arr = []
+theta_chi_arr = []
+cx2_chi_arr = []
+a_prime_arr = []
+a_arr = []
+psi_arr = []
+
+for A_rec in A_recs:
+    data_file = h5pydir + "class_model_data_" + "%.2e" % A_rec + ".hdf5"
+
+    with h5py.File(data_file, "r") as f:
+        tau_data = np.array(f["scalar"]["k=" + str(k)]["tau [Mpc]"])
+        delta_chi_data = np.array(f["scalar"]["k=" + str(k)]["delta_idm_dr"])
+        phi_data = np.array(f["scalar"]["k=" + str(k)]["phi"])
+        kappa_dot_data = np.flip(np.array(f["thermodynamics"]["dmu_idm_dr"]))
+        z_data = np.flip(np.array(f["thermodynamics"]["z"]))
+        thermo_tau_data = np.flip(np.array(f["thermodynamics"]["conf. time [Mpc]"]))
+        cx2_data = np.flip(np.array(f["thermodynamics"]["c_idm_dr^2"]))
+        theta_data = np.array(f["scalar"]["k=" + str(k)]["theta_idm_dr"])
+        theta_dr_data = np.array(f["scalar"]["k=" + str(k)]["theta_idr"])
+        psi_data = np.array(f["scalar"]["k=" + str(k)]["psi"])
+        rho_idr_data = np.array(f["background"]["(.)rho_idr"])
+        rho_idm_data = np.array(f["background"]["(.)rho_idm_dr"])
+        bkg_tau_data = np.array(f["background"]["conf. time [Mpc]"])
+        Pks = np.array(f["power_spectrum"]["Pk"])
+        kks = np.array(f["power_spectrum"]["kk"])
+
+    rho_idr = UnivariateSpline(bkg_tau_data, rho_idr_data, **spline_pars)
+    rho_idm = UnivariateSpline(bkg_tau_data, rho_idm_data, **spline_pars)
+
+    delta_chi = UnivariateSpline(tau_data, delta_chi_data, **spline_pars)
+    delta_chi_dot = delta_chi.derivative()
+    delta_chi_ddot = delta_chi_dot.derivative()
+
+    phi = UnivariateSpline(tau_data, phi_data, **spline_pars)
+    phi_dot = phi.derivative()
+    phi_ddot = phi_dot.derivative()
+
+    theta_chi = UnivariateSpline(tau_data, theta_data, **spline_pars)
+    theta_chi_dot = theta_chi.derivative()
+    theta_dr = UnivariateSpline(tau_data, theta_dr_data, **spline_pars)
+
+    psi = UnivariateSpline(tau_data, psi_data, **spline_pars)
+    cx2_chi = UnivariateSpline(thermo_tau_data, cx2_data, **spline_pars)
+
+    kappa_dot_taus = UnivariateSpline(thermo_tau_data, kappa_dot_data, **spline_pars)
+    kappa_dot_zs = UnivariateSpline(
+        np.flip(z_data), np.flip(kappa_dot_data), **spline_pars
+    )
+    thermo_taus = UnivariateSpline(
+        np.flip(z_data), np.flip(thermo_tau_data), **spline_pars
+    )
+
+    a_data = 1 / (1 + z_data)
+    a = UnivariateSpline(thermo_tau_data, a_data, **spline_pars)
+    aprime = a.derivative()
+
+    delta_chi_arr.append(delta_chi)
+    delta_chi_dot_arr.append(delta_chi_dot)
+    delta_chi_ddot_arr.append(delta_chi_ddot)
+    Pk_arr.append(Pks)
+    kappa_dot_taus_arr.append(kappa_dot_taus)
+    kappa_dot_zs_arr.append(kappa_dot_zs)
+    z_arr.append(z_data)
+    tau_arr.append(tau_data)
+    phi_dot_arr.append(phi_dot)
+    phi_ddot_arr.append(phi_ddot)
+    thermo_tau_arr.append(thermo_taus)
+    theta_dr_arr.append(theta_dr)
+    rho_idr_arr.append(rho_idr)
+    rho_idm_arr.append(rho_idm)
+    theta_chi_arr.append(theta_chi)
+    theta_chi_dot_arr.append(theta_chi_dot)
+    cx2_chi_arr.append(cx2_chi)
+    a_prime_arr.append(aprime)
+    a_arr.append(a)
+    psi_arr.append(psi)
+
+idx = -1
+
+
+def lhs221NonInt(tau):
+    return (
+        3 * phi_ddot_arr[idx](tau)
+        - delta_chi_ddot_arr[idx](tau)
+        - cx2_chi_arr[idx](tau) * k ** 2 * delta_chi_arr[idx](tau)
+        + a_prime_arr[idx](tau) / a_arr[idx](tau) * theta_chi_arr[idx](tau)
+        - k ** 2 * psi_arr[idx](tau)
+    ) / (
+        3 * phi_dot_arr[idx](tau) - delta_chi_dot_arr[idx](tau) - theta_dr_arr[idx](tau)
+    )
+
+
+def rhs221NonInt(tau):
+    return (
+        4
+        / 3
+        * rho_idr_arr[idx](tau)
+        / rho_idm_arr[idx](tau)
+        * -1
+        * kappa_dot_taus_arr[idx](tau)
+    )
+
+
+def lhs221(tau):
+    return quadrature(lhs221NonInt, 6e-2, tau, maxiter=1000)[0]
+
+
+def rhs221(tau):
+    return quadrature(rhs221NonInt, 6e-2, tau, maxiter=200)[0]
+
+
+def plot_delta_recoupling():
+
+    taus = np.linspace(6e-2, 7e-2, 100)
+
+    lhsarr = np.array([lhs221(t) for t in taus])
+    rhsarr = np.array([rhs221(t) for t in taus])
+
+    # 2.21 from paper
+    plt.loglog(taus, -1 * lhsarr, label="LHS 2.21")
+    plt.loglog(taus, -1 * rhsarr, "--", label="RHS 2.21")
+
+    plt.title("Effect of Delta-Like Impulse in Scattering Rate")
+    plt.xlabel("$\\tau$ [Mpc]")
+
+    plt.xlim(6e-2, 7e-2)
+    ax = plt.gca()
+    ax.xaxis.set_minor_locator(MultipleLocator(1e-3))
+    ax.xaxis.set_minor_formatter(NullFormatter())
+    ax.xaxis.set_major_locator(MultipleLocator(1e-2))
+    plt.legend()
+    plot_dir = "Figures/"
+    filename = "Delta_effect.pdf"
+    plt.savefig(plot_dir + filename)
+    plt.clf()
